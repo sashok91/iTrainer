22d38b1e4fbec7e03a121b21111959de3d79a8bd
sashok91 <al.shyrko@gmail.com>
2019-04-19 20:31:52 +0300

    moved user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,61 +1,39 @@
 <?php
 
-/**
- * Created by Reliese Model.
- * Date: Thu, 11 Apr 2019 18:46:44 +0000.
- */
-
 namespace App\Models;
 
-use Reliese\Database\Eloquent\Model as Eloquent;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-/**
- * Class User
- * 
- * @property int $id
- * @property string $name
- * @property string $email
- * @property \Carbon\Carbon $email_verified_at
- * @property string $role
- * @property string $password
- * @property string $api_token
- * @property string $remember_token
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
- * @property string $deleted_at
- * 
- * @property \Illuminate\Database\Eloquent\Collection $questions
- *
- * @package App\Models
- */
-class User extends Eloquent
+class User extends Authenticatable
 {
-	use \Illuminate\Database\Eloquent\SoftDeletes;
+    use Notifiable;
 
-	protected $dates = [
-		'email_verified_at'
-	];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'api_token'
+    ];
 
-	protected $hidden = [
-		'password',
-		'api_token',
-		'remember_token'
-	];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
-	protected $fillable = [
-		'name',
-		'email',
-		'email_verified_at',
-		'role',
-		'password',
-		'api_token',
-		'remember_token'
-	];
-
-	public function questions()
-	{
-		return $this->belongsToMany(\App\Models\Question::class, 'users_questions')
-					->withPivot('id', 'complexity', 'state', 'deleted_at')
-					->withTimestamps();
-	}
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
